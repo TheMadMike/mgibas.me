aae96f20f7d46be000309be7292d88acb069f303
TheMadMike <m.gibas2407@gmail.com>
2020-06-23 13:56:03 +0200

    Add ProjectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="card">
+    <img v-if="imgUrl !== ''" class="mini" :src=imgUrl>
+    <h3>{{ name }}</h3>
+    <p>
+      {{ description }}
+    </p>
+
+    <div class="links">
+      <a v-if=github :href=ghUrl>
+        <i class="fab fa-github-square"></i>
+      </a>
+
+      <a v-if=web :href=webUrl>
+        <i class="fas fa-globe"></i>
+      </a>
+    </div>
+
+    <span class="langs">{{ langs }}</span>
+
+  </div>
+</template>
+
+<script>
+import '../scss/card.scss';
+
+export default {
+  name: 'ProjectCard',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    ghUrl: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
+    webUrl: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
+    imgUrl: {
+      type: String,
+      required: false,
+      default: '',
+    },
+
+    langs: {
+      type: String,
+      required: false,
+    },
+  },
+
+  computed: {
+    github() {
+      return (this.ghUrl !== '');
+    },
+    web() {
+      return (this.webUrl !== '');
+    },
+  },
+};
+</script>
