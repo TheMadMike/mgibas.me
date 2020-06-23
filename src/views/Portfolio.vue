83620460b59eff2a375cc9a13081d14249f0ace1
TheMadMike <m.gibas2407@gmail.com>
2020-06-23 15:37:38 +0200

    Display fetched repos in Porfolio tab

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="portfolio">
     <h1>My projects</h1>
-    <div class="projects">
+    <div class="projects"
+    v-for="n in Math.ceil(projects.length / 3)"
+    :key="n">
       <ProjectCard
-        name="Test"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
-        ghUrl="#"
-        webUrl="#"
+        v-for="project in projects.slice((n-1)*3, ((n-1)*3)+3)"
+        :key="project.id"
+        :name="project.name"
+        :description="project.description"
+        :ghUrl="project.html_url"
+        :webUrl="project.homepage"
       />
     </div>
   </div>
@@ -16,11 +20,22 @@
 import '../scss/portfolio.scss';
 
 import ProjectCard from '../components/ProjectCard.vue';
+import fetchStarredRepos from '../repos';
 
 export default {
   name: 'Portfolio',
   components: {
     ProjectCard,
+  },
+  data() {
+    return {
+      projects: {},
+    };
+  },
+  mounted() {
+    fetchStarredRepos().then((data) => {
+      this.projects = data;
+    });
   },
 };
 </script>
