1536e29e417b0e81779463f31e15db5575e5d2f7
TheMadMike <m.gibas2407@gmail.com>
2020-06-23 16:11:59 +0200

    Add contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form class="contact-form" action="https://mailthis.to/mgibas"
+    method="POST">
+      <input type="text" name="name" placeholder="Name">
+      <input type="email" name="_replyto" placeholder="Email">
+      <textarea name="message" placeholder="Enter your message here"></textarea>
+      <input type="hidden" name="_after" value="https://mgibas.me/">
+      <input type="hidden" name="_honeypot" value="">
+      <input type="hidden" name="_confirmation" value="">
+      <input type="submit" value="Send">
+    </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+};
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="contact">
+    <h2>Contact me</h2>
+    <ContactForm />
   </div>
 </template>
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
+
+import '../scss/contact.scss';
+
 export default {
   name: 'Contact',
+  components: {
+    ContactForm,
+  },
 };
 </script>
